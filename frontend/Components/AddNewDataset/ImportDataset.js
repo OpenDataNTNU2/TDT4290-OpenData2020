ba18a445b6dccdfd986e7963afbf1008af000dc5
Håkon Lia <haakofli@stud.ntnu.no>
2020-10-08 15:11:58 +0200

    added basic form for import dataset

--- frontend/Components/AddNewDataset/ImportDataset.js
+++ frontend/Components/AddNewDataset/ImportDataset.js
@@ -1,7 +1,17 @@
 import { Grid, Button, FormControl, FormLabel, Divider, Snackbar } from '@material-ui/core';
 
+import Input from '../Forms/Input'
+
+import { useState } from 'react'
 
 export default function CreateDataset(){
+
+    const [importUrl, setImportUrl] = useState("");
+
+    const handleChange = (event) => {
+        console.log("clicked")
+        // kjør post request til Components/ApiCalls/PostApi her
+    }
 
     return(
         <Grid
@@ -11,8 +21,15 @@ export default function CreateDataset(){
             alignItems="center"
             style={{ minHeight: '70vh', minWidth: '60vh', marginTop: "5vh"}}
         >
-            <h1>import her</h1>
-
+            
+            <Input 
+                id="importUrl"
+                label="Url som importeres fra"
+                value={importUrl}
+                handleChange={setImportUrl}
+                multiline={false}
+            /><br/>
+            <Button variant="contained" color="primary" onClick={handleChange}>Send inn</Button><br/>
 
         </Grid>
     )
