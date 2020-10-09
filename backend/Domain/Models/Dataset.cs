d11f56bddae906ea7f33b9366203e7dc66d9f5d6
vegasve <vegasve@stud.ntnu.no>
2020-10-09 11:58:06 +0200

    removed some unnecessary whitespaces to clean up some code

--- backend/Domain/Models/Dataset.cs
+++ backend/Domain/Models/Dataset.cs
@@ -11,16 +11,13 @@ namespace OpenData.API.Domain.Models
         public int PublisherId { get; set; }
         public Publisher Publisher { get; set; }
         public IList<Distribution> Distributions { get; set; } = new List<Distribution>();
-
         public EPublicationStatus PublicationStatus { get; set; }
         public EDetailedPublicationStatus DetailedPublicationStatus { get; set; }
         public EAccessLevel AccessLevel { get; set; }
-
         public string TagsIds { get; set; }
         public IList<DatasetTags> DatasetTags { get; set; } = new List<DatasetTags>();
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-
         public int CoordinationId { get; set; }
         public Coordination Coordination { get; set; }
 
