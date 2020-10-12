5db03b78895007c2e339bda102ad01b6fe688370
fridasolheim <32451773+fridasolheim@users.noreply.github.com>
2020-10-12 13:23:33 +0200

    Frontend/detailed dataset feedback (#64)
    * Feedback in order, both button and snackbar
    
    * Fixed minor alignitems bug
    
    * fixes error
    
    Co-authored-by: heddal <44462032+heddal@users.noreply.github.com>
    Co-authored-by: Hedda Mathilde Sæther Langvik <heddalangvik@dhcp-10-22-3-175.wlan.ntnu.no>

--- frontend/pages/DetailedDataset/[id].js
+++ frontend/pages/DetailedDataset/[id].js
@@ -1,15 +1,19 @@
-import { Paper, Grid, Button } from '@material-ui/core';
+import { Paper, Grid, Snackbar } from '@material-ui/core';
 import RequestButtonComp from './RequestButtonComp';
 import { useState } from "react";
-import {PageRender} from '../api/serverSideProps'
 
-// import api functions
+import Alert from '@material-ui/lab/Alert';
+
+import {PageRender} from '../api/serverSideProps'
 import PutApi from '../../Components/ApiCalls/PutApi'
 
 export default function DetailedDataset({data, uri}){
   
-  const [interestCounter, setInterestCounter] = useState(data.interestCounter);
-  
+  const [interestCounter, setInterestCounter] = useState(parseInt(data.interestCounter));
+  const [disabled, setDisabled] = useState(false);
+  // show/hide snackbar with successfull put message
+  const [open, setOpen] = useState(false)
+
   var requestButton;
   var publishedStatus;
 
@@ -19,30 +23,40 @@ export default function DetailedDataset({data, uri}){
       publishedStatus = "Published";
     }
     else {
-      requestButton = <RequestButtonComp handleClick={() => handleChange()}/>;
+      requestButton = <RequestButtonComp handleChange={() => handleChange()} disabled={disabled} />;
       publishedStatus = "Not published";
     }
   }
 
     // puts data into the api with datasets 
     const handleChange = async () => {
-        setInterestCounter(interestCounter + 1);
-
-        //publicationStatus er 0 uansett hvis denne knappen kan trykkes på.
-        //litt usikker på hva detailedPublicationStatus skal stå på hehe. Kan hende vi må mappe over siden den ligger under distributions.
-        const data2 = {
-          "interestCounter" : interestCounter,
-          "identifier" : data.identifier,
-          "title": data.title,
-          "description": data.description,
-          "publisherId": data.publisher.id,
-          "publicationStatus": 1,
-          "detailedPublicationStatus": 0,
-          "categoryId": data.category.id,
-        }
-        
-        PutApi(uri, data2);
-        console.log('Requests er oppdatert!');
+      // setInterestCounter brukes ikke i praksis, oppdaterer manuelt når jeg sender data i put.
+      setInterestCounter(parseInt(interestCounter) + 1);
+      setDisabled(!disabled); 
+      setOpen(true);
+      updateData();
+      
+    }
+    const updateData = async () => {
+      
+      // publicationStatus er 0 uansett hvis denne knappen kan trykkes på.
+      // litt usikker på hva detailedPublicationStatus skal stå på hehe. Kan hende vi må mappe over siden den ligger under distributions.
+      const data2 = {
+        "interestCounter" : interestCounter+1,
+        "identifier" : data.identifier,
+        "title": data.title,
+        "description": data.description,
+        "publisherId": data.publisher.id,
+        "publicationStatus": 1,
+        "detailedPublicationStatus": 0,
+        "categoryId": data.category.id,
+      }
+      /*console.log("Interest counter FØR setInterestCounter: "+ interestCounter);
+      setInterestCounter(interestCounter + 1);*/
+      console.log("Interest counter er nå: "+ data2.interestCounter);
+      setOpen(true);
+      PutApi(uri, data2);
+      console.log('Requests er oppdatert!');
     }
   
 
@@ -50,6 +64,7 @@ export default function DetailedDataset({data, uri}){
         <Grid
             container
             direction="column"
+            alignItems="stretch"
             style={{ minHeight: '70vh', minWidth: '90vh', padding: '5%', border: '2%'}}
         >
             <Grid
@@ -64,26 +79,30 @@ export default function DetailedDataset({data, uri}){
             </Grid>
             
             <Paper variant='outlined' style={{ backgroundColor: '#E1F3FF', padding: '1%' , paddingBottom:'4%'}}>
+              
               <Grid
               container
-              spacing={0}
+              item xs={12} 
               direction="row"
               justify="space-between"
-              >
-                <p><b>Beskrivelse: </b>{data.description}</p>
-                {ifPublished(data.publicationStatus)}{requestButton}
+              alignItems="center"
+            >
+                <span><b>Beskrivelse: </b>{data.description}</span>
+                <span>{ifPublished(data.publicationStatus)} {requestButton}</span>
+                <Snackbar open={open} autoHideDuration={5000} onClose={() => setOpen(false)}>
+                <Alert elevation={1} severity="info">Interesse for datasett registrert</Alert>
+                </Snackbar>
+
               </Grid>
             
               <p><b>Eier:</b> {data.publisher.name}</p>
               <p><b>Type:</b>  {data.distributions.map(distributions => { return (distributions.fileFormat) })} </p>
               <p><b>Publiseringsstatus: </b><i>{ifPublished(data.publicationStatus)}{publishedStatus}</i></p>
               <p><b>Dato publisert: </b> <i>{'Placeholder'}</i></p>
-              <p><b>Link til datasett: </b> {data.distributions.map(distributions => { return (<a href={distributions.uri}> {distributions.uri} </a> )})} </p>
-              
+              <p><b>Link til datasett: </b> {data.distributions.map(distributions => { return (<a key={distributions.id} href={distributions.uri}> {distributions.uri} </a> )})} </p>
             
             </Paper>
-            </Grid>
-
+          </Grid>
     )
 }
 
