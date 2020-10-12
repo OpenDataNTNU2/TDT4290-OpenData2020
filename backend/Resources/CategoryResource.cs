59c0f9dd907460e97656b373d51b2f3b6d4204c7
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2020-10-12 11:28:34 +0200

    Added category filtering on multiple ids in backend. Also added datasets when fetching categories to get the number of datasets per category.

--- backend/Resources/CategoryResource.cs
+++ backend/Resources/CategoryResource.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace OpenData.API.Resources
 {
     public class CategoryResource
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public IList<DatasetResource> Datasets { get; set; } = new List<DatasetResource>();
     }
 }
