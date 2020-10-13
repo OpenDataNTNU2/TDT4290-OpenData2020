5f366710e3aa85a1fb6890698f06292cb51c1a84
Håkon Lia <haakofli@stud.ntnu.no>
2020-10-14 00:20:31 +0200

    added sub categories with expand buttons

--- frontend/Components/Filters/FilterCategory.js
+++ frontend/Components/Filters/FilterCategory.js
@@ -1,10 +1,13 @@
-import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Button } from '@material-ui/core';
+import { Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Button, Grid } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 
 import GetApi from '../ApiCalls/GetApi'
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+
+import CheckboxInput from '../Forms/CheckboxInput'
+
 
 // NB: de ulike filter funksjonene er veldig like atm, dette blir kanskje endret, mtp at vi skal ha under kategorier og slikt
 // så ikke slå de sammen. Vi tar heller en vurdering på det senere.
@@ -13,57 +16,82 @@ export default function FilterCategory(props){
     const [addedFilters, setAddedFilters] = useState([])
     const [categories, setCategories] = useState([])
     const [showItems, setShowItems] = useState(5)
-
-    const [cat, setCat] = useState('')
-
+    const [shownSubItems, setShownSubItems] = useState({});
     const [res, setRes] = useState({})
     
     const handleChange = (event) => {
-        
         let newArr = addedFilters
         newArr.push(event.target.value)
         for(let i = 0; i < newArr.length - 1; i++){
             if(newArr[i] === event.target.value) {newArr.pop(); newArr.splice(i,1)}
         }
         setAddedFilters(newArr)
+
         let newUrlString = ""
         for(let i = 0; i < newArr.length; i++){
             newUrlString += newArr[i] + ','
         }
-
-        props.setUrl(newUrlString)
-        
-
-        
+        props.setUrl(newUrlString)        
     }
         
-        
-      
-    
-    
+    // fetches the categories and places all the top level categories into a list. 
     useEffect(() => {
         GetApi('https://localhost:5001/api/categories', setRes)
-
         let pub = []
         for(let i = 0; i < res.length; i++){
-            pub.push([res[i].name.split(" ")[0], res[i].id, false, i,  res[i].datasets.length]);
+            if(res[i].broader === null) pub.push(res[i])
         }
+        
         setCategories(pub)
-        if(res.length < 5){setShowItems(res.length)}
+        if(pub.length < 5){setShowItems(pub.length)}
     },[props])
 
-    const items = categories.slice(0, showItems).map(category => 
-        <FormControlLabel control={<Checkbox value={category[1]} onChange={handleChange} name={category[0]} />} 
-        label={category[0] + " (" + category[4] + ")"} 
-    />)
+   
 
+    // toggles the sub categories true/false
+    const toggleShownSubItems = id => {
+        setShownSubItems(prevShownSubItems => ({
+        ...prevShownSubItems,
+        [id]: !prevShownSubItems[id]
+        }));
+    };
+
+    
+    // maps all the categories in the list sent in, 
+    // then checks if an element in the list have elements in the narrower field
+    // if that is true, it runs this function again, but with the narrower list instead. 
+    
+    const items = (cats) => cats.map((category) => (
+            <div>
+                {category.narrower.length === 0 ? 
+                    <CheckboxInput handleChange={handleChange} id={category.id} name={category.name} />
+                
+                :   <div>
+                        <CheckboxInput handleChange={handleChange} id={category.id} name={category.name} />
+                        {!shownSubItems[category.id] ? 
+                            <ExpandMoreIcon style={{cursor:'pointer'}} fontSize="small" onClick={() => toggleShownSubItems(category.id)} />
+                        :   <ExpandLessIcon style={{cursor:'pointer'}} fontSize="small" onClick={() => toggleShownSubItems(category.id)} />
+                        }
+                        
+                        <div style={{marginLeft: "3vh"}} hidden={!shownSubItems[category.id]} id={category.id} >
+                            {items(category.narrower)}
+                        </div>
+                        
+                    </div>
+                }
+            </div>  
+        )
+    )
+        
     return(
         <div>
             <FormControl>
                 <FormLabel>Velg kategori</FormLabel>
                 <FormGroup >
                     <br/>
-                    {items}
+
+                    {items(categories)}
+                    
                     {showItems === 5
                     ? <ExpandMoreIcon style={{cursor:'pointer'}} onClick={() => setShowItems(categories.length)} fontSize="large"/> 
                     : categories.length > 5 
