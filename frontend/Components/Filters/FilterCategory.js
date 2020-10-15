c80e3c4e326938448ee550a57fb5b22042706c51
Håkon Lia <haakofli@stud.ntnu.no>
2020-10-15 13:01:19 +0200

    added select for coordination/datasets in index and radiobuttons for status in CreateCoordinations

--- frontend/Components/Filters/FilterCategory.js
+++ frontend/Components/Filters/FilterCategory.js
@@ -11,95 +11,95 @@ import CheckboxInput from '../Forms/CheckboxInput'
 
 // NB: de ulike filter funksjonene er veldig like atm, dette blir kanskje endret, mtp at vi skal ha under kategorier og slikt
 // så ikke slå de sammen. Vi tar heller en vurdering på det senere.
-export default function FilterCategory(props){
+export default function FilterCategory(props) {
 
     const [addedFilters, setAddedFilters] = useState([])
     const [categories, setCategories] = useState([])
     const [showItems, setShowItems] = useState(5)
     const [shownSubItems, setShownSubItems] = useState({});
     const [res, setRes] = useState({})
-    
+
     const handleChange = (event) => {
         let newArr = addedFilters
         newArr.push(event.target.value)
-        for(let i = 0; i < newArr.length - 1; i++){
-            if(newArr[i] === event.target.value) {newArr.pop(); newArr.splice(i,1)}
+        for (let i = 0; i < newArr.length - 1; i++) {
+            if (newArr[i] === event.target.value) { newArr.pop(); newArr.splice(i, 1) }
         }
         setAddedFilters(newArr)
 
         let newUrlString = ""
-        for(let i = 0; i < newArr.length; i++){
+        for (let i = 0; i < newArr.length; i++) {
             newUrlString += newArr[i] + ','
         }
-        props.setUrl(newUrlString)        
+        props.setUrl(newUrlString)
     }
-        
+
     // fetches the categories and places all the top level categories into a list. 
     useEffect(() => {
-        
+
         GetApi('https://localhost:5001/api/categories', setRes)
         let pub = []
-        for(let i = 0; i < res.length; i++){
-            if(res[i].broader === null) pub.push(res[i])
+        for (let i = 0; i < res.length; i++) {
+            if (res[i].broader === null) pub.push(res[i])
         }
         setCategories(pub)
-        
-        
-    },[props])
 
-   
+
+    }, [props])
+
+
 
     // toggles the sub categories true/false
     const toggleShownSubItems = id => {
         setShownSubItems(prevShownSubItems => ({
-        ...prevShownSubItems,
-        [id]: !prevShownSubItems[id]
+            ...prevShownSubItems,
+            [id]: !prevShownSubItems[id]
         }));
     };
 
-    
+
     // maps all the categories in the list sent in, 
     // then checks if an element in the list have elements in the narrower field
     // if that is true, it runs this function again, but with the narrower list instead. 
-    
+
     const items = (cats) => cats.map((category) => (
-            <div>
-                {category.narrower.length === 0 ? 
+        <div>
+            {category.narrower.length === 0 ?
+                <CheckboxInput handleChange={handleChange} id={category.id} name={category.name + " (" + category.datasetsCount + ")"} />
+
+                : <div>
                     <CheckboxInput handleChange={handleChange} id={category.id} name={category.name + " (" + category.datasetsCount + ")"} />
-                
-                :   <div>
-                        <CheckboxInput handleChange={handleChange} id={category.id} name={category.name + " (" + category.datasetsCount + ")"} />
-                        {!shownSubItems[category.id] ? 
-                            <ExpandMoreIcon style={{cursor:'pointer'}} fontSize="small" onClick={() => toggleShownSubItems(category.id)} />
-                        :   <ExpandLessIcon style={{cursor:'pointer'}} fontSize="small" onClick={() => toggleShownSubItems(category.id)} />
-                        }
-                        
-                        <div style={{marginLeft: "2vh"}} hidden={!shownSubItems[category.id]} id={category.id} >
-                            {items(category.narrower)}
-                        </div>
-                        
+                    {!shownSubItems[category.id] ?
+                        <ExpandMoreIcon style={{ cursor: 'pointer' }} fontSize="small" onClick={() => toggleShownSubItems(category.id)} />
+                        : <ExpandLessIcon style={{ cursor: 'pointer' }} fontSize="small" onClick={() => toggleShownSubItems(category.id)} />
+                    }
+
+                    <div style={{ marginLeft: "2vh" }} hidden={!shownSubItems[category.id]} id={category.id} >
+                        {items(category.narrower)}
                     </div>
-                }
-            </div>  
-        )
+
+                </div>
+            }
+        </div>
     )
-        
-    return(
+    )
+
+    return (
         <div>
             <FormControl>
-                <FormLabel>Velg kategori</FormLabel>
+                <FormLabel>Kategori</FormLabel>
                 <FormGroup >
-                    <br/>
+                    <br />
 
                     {items(categories.slice(0, showItems))}
-                    
+
                     {showItems === 5 && categories.length > 5
-                    ? <ExpandMoreIcon style={{cursor:'pointer'}} onClick={() => setShowItems(categories.length)} fontSize="large"/> 
-                    : categories.length > 5 
-                    ? <ExpandLessIcon style={{cursor:'pointer'}} onClick={() => setShowItems(5)} fontSize="large" />
-                    : null
+                        ? <ExpandMoreIcon style={{ cursor: 'pointer' }} onClick={() => setShowItems(categories.length)} fontSize="large" />
+                        : categories.length > 5
+                            ? <ExpandLessIcon style={{ cursor: 'pointer' }} onClick={() => setShowItems(5)} fontSize="large" />
+                            : null
                     }
-                    
+
                 </FormGroup>
             </FormControl>
         </div>
