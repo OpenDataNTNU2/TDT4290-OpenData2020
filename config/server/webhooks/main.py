836de0f8d83ff1b81babb9ce053600f66644b4d5
Odin Vatne <odinjv@stud.ntnu.no>
2020-11-09 05:11:33 +0100

    Fixed webhook script, used cheaper npm install for  quicker deployment

--- config/server/webhooks/main.py
+++ config/server/webhooks/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, Response
 from subprocess import run
 import logging
+import threading
+import atexit
 
 PORT = 8080
 TRACKED_BRANCHES = ('dev')
+threads = []
 
 logging.basicConfig(
     filename='log.txt',
@@ -11,6 +14,44 @@ logging.basicConfig(
     format='WebhookListener %(asctime)s %(levelname)-8s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+
+class serverUpdateRunner (threading.Thread):
+    def __init__(self, branch):
+        threading.Thread.__init__(self)
+        self.branch = branch
+
+    def run(self):
+        threads.append(self)
+        branch = self.branch
+
+        logging.info('Stopping old server processes...')
+        result = run('/home/student/dev/config/server/scripts/stop.sh')
+        if result.returncode != 0:
+            logging.error('Failed to stop old processes. Exit code {}'.format(result.returncode))
+            return
+
+        logging.info('Pulling latest changes...')
+        result = run(['/home/student/dev/config/server/scripts/update.sh', branch])
+        if result.returncode != 0:
+            logging.error('Failed to pull from repo. Exit code {}'.format(result.returncode))
+            return
+
+        logging.info('Building files...')
+        result = run(['/home/student/dev/config/server/scripts/build.sh', branch])
+        if result.returncode != 0:
+            logging.error('Failed to build frontend/backend. Exit code {}'.format(result.returncode))
+            return
+
+        logging.info('Starting server...')
+        result = run(['/home/student/dev/config/server/scripts/run.sh', branch])
+        logging.info('Started sever.')
+        return
+
+def join_threads():
+    for thread in threads:
+        thread.join()
+
+atexit.register(join_threads)
 
 app = Flask(__name__)
 
@@ -23,42 +64,23 @@ def respond():
             return Response(status=200, response='Request intentionally ignored.')
 
         data = request.json
-        logging.info('Git push detected. {} new commit(s) pushed to {} by {}.'.format(len(data.commits), data.ref, data.pusher.name))
+        logging.info('Git push detected. {} new commit(s) pushed to {} by {}.'.format(len(data['commits']), data['ref'], data['pusher']['name']))
 
-        if not data.ref.startswith('refs/head/'):
+        if not data['ref'].startswith('refs/heads/'):
             logging.info('Push message format not as expected')
             return Response(status=400, response='Push message format not as expected')
 
-        branch = data.ref[len('refs/head/'):]
+        branch = data['ref'][len('refs/heads/'):]
         if branch not in TRACKED_BRANCHES:
             logging.info('Push was not to tracked branch. No further action will be taken.')
             return Response(status=200, response='Push was not to tracked branch. No further action will be taken.')
 
-        logging.info('Stopping old server processes...')
-        result = run('/home/student/dev/config/server/scripts/stop.sh')
-        if result.returncode != 0:
-            logging.error('Failed to stop old processes. Exit code {}'.format(result.returncode))
-            return Response(status=500, response='Failed to stop old processes. Exit code {}'.format(result.returncode))
+        updater = serverUpdateRunner(branch)
+        updater.start()
 
-        logging.info('Pulling latest changes...')
-        result = run('/home/student/dev/config/server/scripts/update.sh ' + branch)
-        if result.returncode != 0:
-            logging.error('Failed to pull from repo. Exit code {}'.format(result.returncode))
-            return Response(status=500, response='Failed to pull from repo. Exit code {}'.format(result.returncode))
+        return Response(status=200, response='Server update started for branch {}.'.format(branch))
 
-        logging.info('Building files...')
-        result = run('/home/student/dev/config/server/scripts/build.sh ' + branch)
-        if result.returncode != 0:
-            logging.error('Failed to build frontend/backend. Exit code {}'.format(result.returncode))
-            return Response(status=500, response='Failed to build frontend/backend. Exit code {}'.format(result.returncode))
-
-        logging.info('Starting server...')
-        result = run('/home/student/dev/config/server/scripts/run.sh ' + branch)
-        logging.info('Started sever.')
-
-        return Response(status=200, response='Server updated successfully.')
-
-    except e:
+    except Exception as e:
         logging.error(e)
         logging.info('Headers: {}'.format(request.headers))
         logging.info('Payload: {}'.format(request.json))
